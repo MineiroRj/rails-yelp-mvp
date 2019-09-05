3fe4e4de4eb8fd539f7a6e591f439fc293ecc894
Fernando Martins <fernando@fdonline.com.br>
2019-09-05 09:53:25 -0300

    part of the exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+  # get 'restaurants/:restaurant_id/reviews', to: 'reviews#index'
+  # get 'reviews/:id', to: 'reviews#show'
+  # get 'reviews/:id/edit', to: 'reviews#edit'
+
+  resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,49 @@
+
+category = Restaurant::CATEGORY
+
+  Restaurant.create(
+    {
+      name: 'Maya',
+      address: 'NEX',
+      phone_number: '2222-4444',
+      category: category.sample,
+    }
+  )
+
+  Restaurant.create(
+    {
+      name: 'Maya',
+      address: 'NEX',
+      phone_number: '2222-4444',
+      category: category.sample,
+    }
+  )
+
+  Restaurant.create(
+    {
+      name: 'Maya',
+      address: 'NEX',
+      phone_number: '2222-4444',
+      category: category.sample,
+    }
+  )
+
+  Restaurant.create(
+    {
+      name: 'Maya',
+      address: 'NEX',
+      phone_number: '2222-4444',
+      category: category.sample,
+    }
+  )
+  Restaurant.create(
+    {
+      name: 'Maya',
+      address: 'NEX',
+      phone_number: '2222-4444',
+      category: category.sample,
+    }
+  )
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
